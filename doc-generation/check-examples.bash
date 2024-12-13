509e9c753559d84479b042e1b2c3339cf8f6a09e
Arne Broering <arne.broering@siemens.com>
2024-12-13 15:58:57 +0100

    feat(app-definition): add LinkML spec
    Signed-off-by: Arne Broering <arne.broering@siemens.com>
    Co-authored-by: Silvano Cirujano Cuesta <silvano.cirujano-cuesta@siemens.com>

--- doc-generation/check-examples.bash
+++ doc-generation/check-examples.bash
@@ -11,21 +11,29 @@ CONFIGS="${DOCS_GEN}/configurations"
 
 gen_spec () {
     SPEC_ROOT="${ROOT_DIR}/$(jq -r '.root' "${CONFIGS}/$1")"
-    echo "${SPEC_ROOT}"
+    echo "Spec root folder: ${SPEC_ROOT}"
+
+    TARGET_CLASS="$(jq -r '.targetclass' "${CONFIGS}/$1")"
+    echo "Target class: ${TARGET_CLASS}"
+
+    SCHEMA_FILE="$(jq -r '.schemafile' "${CONFIGS}/$1")"
+    echo "Schema file: ${SCHEMA_FILE}"
 
     EXAMPLES_DIR="${SPEC_ROOT}/examples/valid"
-    for EXAMPLE in $(ls "${EXAMPLES_DIR}"/DesiredState-0*.yaml) ; do
+    echo "Examples folder: ${EXAMPLES_DIR}"
+    for EXAMPLE in $(ls "${EXAMPLES_DIR}"/*-0*.yaml) ; do
+        echo "Validate example: ${EXAMPLE}"
          ${RUN} linkml validate \
-            --schema "${SPEC_ROOT}/desired-state.linkml.yaml" \
-            --target-class DesiredState \
+            --schema "${SPEC_ROOT}/${SCHEMA_FILE}" \
+            --target-class "${TARGET_CLASS}" \
             "${EXAMPLE}"
     done
 
     COUNTEREXAMPLES_DIR="${SPEC_ROOT}/examples/invalid"
-    for COUNTEREXAMPLE in $(ls "${COUNTEREXAMPLES_DIR}"/DesiredState-0*.yaml) ; do
+    for COUNTEREXAMPLE in $(ls "${COUNTEREXAMPLES_DIR}"/*-0*.yaml) ; do
         ${RUN} linkml validate \
-            --schema "${SPEC_ROOT}/desired-state.linkml.yaml" \
-            --target-class DesiredState \
+            --schema "${SPEC_ROOT}/${SCHEMA_FILE}" \
+            --target-class "${TARGET_CLASS}" \
             "${COUNTEREXAMPLE}" \
                 && echo "ERROR: Validation of 'examples/invalid/${COUNTEREXAMPLE}' was expected to fail, but has succeeded." \
                 || echo "ok: validation failed, as expected"
@@ -36,7 +44,7 @@ gen_spec () {
     # https://github.com/linkml/linkml/issues/2425
     if false ; then
     ${RUN} linkml examples \
-        --schema "${SPEC_ROOT}/desired-state.linkml.yaml" \
+        --schema "${SPEC_ROOT}/*.linkml.yaml" \
         --input-directory "${SPEC_ROOT}/examples/valid" \
         --counter-example-input-directory "${SPEC_ROOT}/examples/invalid" \
         --output-directory "${SPEC_ROOT}/output"
